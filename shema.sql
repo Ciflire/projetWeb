6847c53aefa9246d487a054500a9d6d228c02fb2
VESSE Léo <leovesse@gmail.com>
2024-01-05 16:05:41 +0100

    recovered schema from db

---
+++ shema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS "users" (
+        "id_user"       INTEGER NOT NULL UNIQUE,
+        "name"  TEXT NOT NULL,
+        "admin" INTEGER NOT NULL,
+        "password"      TEXT NOT NULL,
+        "hash"  TEXT NOT NULL,
+        "profile_picture"       INTEGER,
+        PRIMARY KEY("id_user" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "challenges" (
+        "id_challenge"  INTEGER NOT NULL UNIQUE,
+        "name"  TEXT NOT NULL,
+        "creation_date" INTEGER NOT NULL,
+        "end_date"      INTEGER,
+        PRIMARY KEY("id_challenge" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "validations" (
+        "id_user"       INTEGER NOT NULL,
+        "id_challenge"  INTEGER NOT NULL,
+        "inscription_date"      INTEGER NOT NULL,
+        "validation_date"       INTEGER,
+        "token" TEXT NOT NULL,
+        FOREIGN KEY("id_challenge") REFERENCES "challenges"("id_challenge"),
+        FOREIGN KEY("id_user") REFERENCES "users"("id_user"),
+        PRIMARY KEY("id_user","id_challenge")
+);
+/* No STAT tables available */
+
