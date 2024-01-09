e32e97a3d8fa1f25b988e99343d3f365cde8397f
VESSE Léo <leovesse@gmail.com>
2024-01-09 13:36:43 +0100

    login achieved

--- main.py
+++ main.py
@@ -35,15 +35,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-# DB related
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource("schema.sql", mode="r") as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
 def get_db():
     db = sqlite3.connect(app.config["DATABASE"])
     db.row_factory = sqlite3.Row
@@ -57,46 +48,113 @@ def close_db(error):
 
 
 class User(UserMixin):
-    def __init__(self, username, password):
-        self.id = user_id
-        self.username = username
+    def __init__(
+        self,
+        user_id: int,
+        name: str,
+        is_admin: int,
+        password: str,
+    ):
+        self.user_id = user_id
+        self.name = name
+        self.is_admin = is_admin
         self.password = password
+
+    def __str__(self):
+        return "{0}".format(self.name)
+
+    def serialize(self):
+        """Méthode de formatage pour ajouter les données dans la DB"""
+        return (
+            self.user_id,
+            self.name,
+            self.is_admin,
+            self.password,
+        )
+
+    def get_id(self):  # necessary for flask_login
+        return self.user_id
 
 
 @login_manager.user_loader
-def load_user(user_id):
-    db = get_db()
-    user_data = db.execute(
-        "SELECT * FROM user WHERE id = ?", (user_id,)
-    ).fetchone()
-    if user_data:
-        return User(user_data["id"], user_data["username"])
+def load_user(user_id: int):
+    cursor = get_db().cursor()
+    cursor.execute(
+        "SELECT id_user, name, admin, hash FROM users WHERE id_user = ?",
+        (user_id,),
+    )
+    db_data = cursor.fetchone()
+    if db_data is not None:
+        client = User(*db_data)
+        return client
     return None
 
 
-user = User("admin", "password")
+@app.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        name = request.form["username"]
+        password = request.form["password"]
+        password_confirm = request.form["confirm_password"]
+        conn = get_db()
+        cursor = conn.cursor()
+        if password != password_confirm:  # passwords do not match
+            return render_template(
+                "popup.html",
+                message="Les mots de passe ne correspondent pas.",
+            )
+        hashed_pass = generate_password_hash(password)
+        cursor.execute(
+            "INSERT INTO users (name, admin, hash) VALUES (?,1,?)",
+            (name, hashed_pass),
+        )
+        conn.commit()
+        return redirect(url_for("login"))
+    return render_template("register.html")
 
-login_manager.user_loader(load_user)
+
+@app.route("/login/", methods=("GET", "POST"))
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        conn = get_db()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id_user, name, admin, hash FROM users WHERE name = ?",
+            (username,),
+        )
+        user = cursor.fetchone()
+        if user is None:
+            return render_template(
+                "popup.html",
+                message="Cette adresse email n'est liée à aucun compte.",
+            )
+
+        client = User(*user)
+        if not check_password_hash(client.password, password):
+            return render_template(
+                "popup.html", message="Le mot de passe est incorrect."
+            )
+        login_user(client)
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("profile/1"))
 
 
-def connect_db():
-    return sqlite3.connect(app.config["DATABASE"])
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 
 def generateToken() -> str:
     token = secrets.token_hex(16)
     return token
-
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-
-@app.teardown_request
-def teardown_request(exception):
-    if hasattr(g, "db"):
-        g.db.close()
 
 
 # accueil
@@ -153,78 +211,6 @@ def deleteChallenge():
 @app.route("/template")
 def template():
     return render_template("template.html")
-
-
-login_manager.user_loader(load_user)
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if current_user.is_authenticated:
-        return redirect("/profile")
-
-    if request.method == "POST":
-        # Get the username and password from the request
-        username = request.form["username"]
-        password = request.form["password"]
-
-        # Check if the username and password are valid
-        if user.username == username and user.password == password:
-            # Login the user
-            login_user(user)
-            return redirect("")
-
-        # Otherwise, show an error message
-        return render_template(
-            "login.html", error="Invalid username or password."
-        )
-
-    # Render the login form for GET requests
-    return render_template("login.html")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-
-        # Utilisez SHA-256 pour hacher le mot de passe
-        hashed_password = generate_password_hash(password)
-
-        cursor = g.db.cursor()
-        cursor.execute(
-            "INSERT INTO users (name, admin, password, hash) VALUES (?, 1, ?, ?)",
-            (username, hashed_password, hashed_password),
-        )
-        g.db.commit()
-
-        flash("Your account has been created!", "success")
-        return redirect(url_for("login"))
-
-    return render_template("register.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-
-        cursor = g.db.cursor()
-        cursor.execute("SELECT * FROM users WHERE name = ?", (username,))
-        user = cursor.fetchall()
-        if user and (check_password_hash(user[0][3], password)):
-            session["user_id"] = user[0]
-            return render_template("popup.html", message="Connexion reussie")
-        else:
-            render_template(
-                "popup.html",
-                message="""
-                Login unsuccessful. Please check your username and password.""",
-            )
-
-    return render_template("connexion.html")
 
 
 @app.route("/createchallenge")
@@ -365,7 +351,7 @@ def user_json(id_user):
 def all_users():
     conn = sqlite3.connect(app.config["DATABASE"])
     cur = conn.cursor()
-    cur.execute(f"""SELECT id_user,name FROM users;""")
+    cur.execute("""SELECT id_user,name FROM users;""")
     info = cur.fetchall()
     conn.close()
     # print(info, file=sys.stderr)
